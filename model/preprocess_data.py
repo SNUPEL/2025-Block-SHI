1e241fe3e8a7515312e47a85e03955e3f8a03611
Hong Lee <hally2@snu.ac.kr>
2025-05-20 13:44:22 +0900

    Add code
    #후처리, 전처리 통합

--- model/preprocess_data.py
+++ model/preprocess_data.py
@@ -15,8 +15,8 @@ def get_work_area_list(df_work_area_group, df_work_area):
             filtered_df_work_area['그룹내정반위치X'] = filtered_df_work_area['그룹내정반위치X'] - filtered_df_work_area[
                 '마진거리4'].fillna(0)
 
-            filtered_df_work_area['그룹내정반위치X'] = filtered_df_work_area['그룹내정반위치X'] - min(
-                filtered_df_work_area['그룹내정반위치X'])
+            min_x_of_work_area = min(filtered_df_work_area['그룹내정반위치X'])
+            filtered_df_work_area['그룹내정반위치X'] = filtered_df_work_area['그룹내정반위치X'] - min_x_of_work_area
 
             filtered_df_work_area['그룹내정반위치Y'] = -1 * filtered_df_work_area['그룹내정반위치Y']
 
@@ -42,7 +42,8 @@ def get_work_area_list(df_work_area_group, df_work_area):
                                               indoor_outdoor_condition=row['옥내외'], lug_condition=row['러그고려'],
                                               L_limit_of_block=row['사이즈제한LTH'], B_limit_of_block=row['사이즈제한BTH'],
                                               H_limit_of_block=row['사이즈제한HGT'], W_limit_of_block=row['사이즈제한WGT'],
-                                              TP_condition=row['TP운송여부'], TP_direction=direction, L=length, B=breadth)
+                                              TP_condition=row['TP운송여부'], TP_direction=direction, L=length, B=breadth,
+                                              min_x_of_work_area=min_x_of_work_area)
                     temp_work_area.work_unit_dict[rect['정반ID']] = WorkUnit(unit_id=rect['정반ID'], x=rect['그룹내정반위치X'],
                                                                            y=rect['그룹내정반위치Y'], dx=length, dy=breadth)
                     work_area_dict[(row['그룹ID'], rect['정반ID'])] = temp_work_area
@@ -87,7 +88,7 @@ def get_work_area_list(df_work_area_group, df_work_area):
                                    L_limit_of_block=row['사이즈제한LTH'], B_limit_of_block=row['사이즈제한BTH'],
                                    H_limit_of_block=row['사이즈제한HGT'], W_limit_of_block=row['사이즈제한WGT'],
                                    TP_condition=row['TP운송여부'], TP_direction=direction,
-                                   L=combined_length, B=combined_breadth)
+                                   L=combined_length, B=combined_breadth, min_x_of_work_area = min_x_of_work_area)
 
                     print(
                         f"work_area {temp_work_area.group_id}-{temp_work_area.surface_id_list}:"
@@ -111,8 +112,8 @@ def get_work_area_list(df_work_area_group, df_work_area):
                         missing_breadth = y_vals[max_y_idx + 1] - y_vals[min_y_idx]
 
                         # 향후 고정 블록으로 구현
-                        temp_work_area.add_unavailable_area(missing_lower_left_x, missing_lower_left_y,
-                                                            missing_length, missing_breadth)
+                        temp_work_area.add_unavailable_area(missing_lower_left_x, missing_lower_left_y, missing_length,
+                                                            missing_breadth)
 
                         print(
                             f"work_area {temp_work_area.group_id}-{temp_work_area.surface_id_list}"
@@ -128,61 +129,6 @@ def get_work_area_list(df_work_area_group, df_work_area):
 
 def preprocess_data(self):
     sheet_name_list = list(self.df_raw_data_dict.keys())
-    if 'WORKAREA_GROUP' in sheet_name_list and 'WORKAREA' in sheet_name_list:
-        df_work_area_group = self.df_raw_data_dict['WORKAREA_GROUP']
-        df_work_area = self.df_raw_data_dict['WORKAREA']
-        self.work_area_dict = get_work_area_list(df_work_area_group, df_work_area)
-        print('WorkArea class has been defined')
-    else:
-        print('Sheet names do not match')
-
-    # if 'WORKAREA_CRANE_REL' in sheet_name_list and 'CRANE_WORK_TIME' in sheet_name_list:
-    #     df_crane_time = pd.merge(self.df_raw_data_dict['WORKAREA_CRANE_REL'], self.df_raw_data_dict['CRANE_WORK_TIME'],
-    #                              on=['작업종류', '크레인ID'], how='left')
-    #     for _, work_area in self.work_area_dict.items():
-    #         for key, group in df_crane_time.groupby('그룹ID').get_group(work_area.group_id).groupby('작업종류'):
-    #             work_area.crane_operation_dict[key] = (int(sum(group['작업시간'] * 2)), list(group['크레인ID']))
-    #     print('Crane operation dictionary has been defined')
-    # else:
-    #     print('Sheet names do not match')
-    # TO와 OUT을 합치기 위해 수정
-    if 'WORKAREA_CRANE_REL' in sheet_name_list and 'CRANE_WORK_TIME' in sheet_name_list:
-        df_crane_time = pd.merge(self.df_raw_data_dict['WORKAREA_CRANE_REL'], self.df_raw_data_dict['CRANE_WORK_TIME'],
-                                 on=['작업종류', '크레인ID'], how='left')
-
-        out_to_types = ['OUT', 'TO']
-
-        for _, work_area in self.work_area_dict.items():
-            # 그룹ID로 먼저 필터링
-            group_df = df_crane_time[df_crane_time['그룹ID'] == work_area.group_id]
-
-            # 일반 작업종류 처리
-            for key, group in group_df[~group_df['작업종류'].isin(out_to_types)].groupby('작업종류'):
-                work_area.crane_operation_dict[key] = (int(sum(group['작업시간'] * 2)), list(group['크레인ID']))
-
-            # PE와 TO 작업을 합쳐서 처리
-            pe_to_df = group_df[group_df['작업종류'].isin(out_to_types)]
-            if not pe_to_df.empty:
-                # 합쳐진 작업시간 계산
-                combined_time = int(sum(pe_to_df['작업시간'] * 2))
-                # 두 작업에 사용된 크레인ID 리스트 (중복 제거)
-                combined_cranes = list(pe_to_df['크레인ID'].unique())
-                # TO 키 값으로 저장
-                work_area.crane_operation_dict['TO'] = (combined_time, combined_cranes)
-
-        print('Crane operation dictionary has been defined with PE and TO combined')
-    else:
-        print('Sheet names do not match')
-
-    if 'CRANE' in sheet_name_list and 'CRANE_RESV_SCH' in sheet_name_list:
-        for _, row in self.df_raw_data_dict['CRANE'].iterrows():
-            self.crane_dict[row['크레인ID']] = Crane(Crane_id=row['크레인ID'], condition=row['사용여부'])
-
-        # 향후 크레인 별 예약 작업 list 추가하는 코드 구현
-        # 더미 변수 생성해 step function에 pulse 추가
-        print('Crane class has been defined')
-    else:
-        print('Sheet names do not match')
 
     if 'UNAL_WORKDAY' in sheet_name_list:
         # 향후 추가해 일정으로 활용
@@ -236,6 +182,66 @@ def preprocess_data(self):
     else:
         print('Sheet names do not match')
 
+    if 'WORKAREA_GROUP' in sheet_name_list and 'WORKAREA' in sheet_name_list:
+        df_work_area_group = self.df_raw_data_dict['WORKAREA_GROUP']
+        df_work_area = self.df_raw_data_dict['WORKAREA']
+        self.work_area_dict = get_work_area_list(df_work_area_group, df_work_area)
+        print('WorkArea class has been defined')
+    else:
+        print('Sheet names do not match')
+
+    # if 'WORKAREA_CRANE_REL' in sheet_name_list and 'CRANE_WORK_TIME' in sheet_name_list:
+    #     df_crane_time = pd.merge(self.df_raw_data_dict['WORKAREA_CRANE_REL'], self.df_raw_data_dict['CRANE_WORK_TIME'],
+    #                              on=['작업종류', '크레인ID'], how='left')
+    #     for _, work_area in self.work_area_dict.items():
+    #         for key, group in df_crane_time.groupby('그룹ID').get_group(work_area.group_id).groupby('작업종류'):
+    #             work_area.crane_operation_dict[key] = (int(sum(group['작업시간'] * 2)), list(group['크레인ID']))
+    #     print('Crane operation dictionary has been defined')
+    # else:
+    #     print('Sheet names do not match')
+    # TO와 OUT을 합치기 위해 수정
+    if 'WORKAREA_CRANE_REL' in sheet_name_list and 'CRANE_WORK_TIME' in sheet_name_list:
+        df_crane_time = pd.merge(self.df_raw_data_dict['WORKAREA_CRANE_REL'], self.df_raw_data_dict['CRANE_WORK_TIME'],
+                                 on=['작업종류', '크레인ID'], how='left')
+
+        out_to_types = ['OUT', 'TO']
+
+        for _, work_area in self.work_area_dict.items():
+            # 그룹ID로 먼저 필터링
+            group_df = df_crane_time[df_crane_time['그룹ID'] == work_area.group_id]
+
+            # 일반 작업종류 처리
+            for key, group in group_df[~group_df['작업종류'].isin(out_to_types)].groupby('작업종류'):
+                work_area.crane_operation_dict[key] = (int(sum(group['작업시간'] * 2)), list(group['크레인ID']))
+
+            # PE와 TO 작업을 합쳐서 처리
+            pe_to_df = group_df[group_df['작업종류'].isin(out_to_types)]
+            if not pe_to_df.empty:
+                # 합쳐진 작업시간 계산
+                combined_time = int(sum(pe_to_df['작업시간'] * 2))
+                # 두 작업에 사용된 크레인ID 리스트 (중복 제거)
+                combined_cranes = list(pe_to_df['크레인ID'].unique())
+                # TO 키 값으로 저장
+                work_area.crane_operation_dict['TO'] = (combined_time, combined_cranes)
+
+        print('Crane operation dictionary has been defined with PE and TO combined')
+    else:
+        print('Sheet names do not match')
+
+    if 'CRANE' in sheet_name_list and 'CRANE_RESV_SCH' in sheet_name_list:
+        df_crane = self.df_raw_data_dict['CRANE']
+        df_unavailable_crane = self.df_raw_data_dict['CRANE_RESV_SCH']
+        for _, row in df_crane.iterrows():
+            self.crane_dict[row['크레인ID']] = Crane(Crane_id=row['크레인ID'], condition=row['사용여부'])
+        for _, row in df_unavailable_crane.iterrows():
+            self.crane_dict[row['크레인ID']].unavailable_time_dict[row['예약작업이름']] =\
+                self.calendar_dict[pd.to_datetime(row['예약작업시간'])]
+        # 향후 크레인 별 예약 작업 list 추가하는 코드 구현
+        # 더미 변수 생성해 step function에 pulse 추가
+        print('Crane class has been defined')
+    else:
+        print('Sheet names do not match')
+
     if 'BLK' in sheet_name_list:
         df_block = self.df_raw_data_dict['BLK']
         df_block[['착수일', '완료일', 'TO일정', 'PE일정']] \
@@ -271,7 +277,10 @@ def preprocess_data(self):
         print('Sheet names do not match')
 
     if 'UNAL_WORKAREA' in sheet_name_list:
-        # 향후 추가해 더미블록으로 구현
-        pass
+        df_unavailable_workarea = self.df_raw_data_dict['UNAL_WORKAREA']
+        for _, row in df_unavailable_workarea.iterrows():
+            for key, value in self.work_area_dict.items():
+                if row['그룹ID'] == key[0] and row['정반ID'] in list(key[1]):
+                    value.work_unit_dict.unavailable_duration_list.append([row['정반불가시작일'], row['정반불가종료일']])
     else:
         print('Sheet names do not match')
