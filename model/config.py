ec53660f7bb1e486c563c03feb9197b60fcc5689
Hong Lee <hally2@snu.ac.kr>
2025-06-26 17:27:45 +0900

    Add code
    삼성 제안 코드 추가

--- model/config.py
+++ model/config.py
@@ -8,15 +8,15 @@ def create_config():
 
     config['data_file_path'] = '../data/data_rev0.2.xlsx'
 
-    config['data_start_date'] = '2019-04-20'  # 시작일자 (착수일 기준)
+    config['data_start_date'] = '2019-05-30'  # 시작일자 (착수일 기준)
     config['only_workingday'] = False  # True: workingday 기준 duration의 기간을 셈, False: data_end_date 사용
     config['data_duration'] = 40  # workingday
-    config['data_end_date'] = '2019-05-30'
-    config['time_limit'] = 1800  # 탐색 시간
+    config['data_end_date'] = '2019-08-23'
+    config['time_limit'] = 30  # 탐색 시간
 
     # 배치 확정된 블록 사용
     config['use_block_allocation_result'] = False  # True 시 하단의 결과를 가져옴
-    config['result_data_file_path'] = '../results/20250611_14h_35m_8s/block_allocation_result.xlsx'
+    config['result_data_file_path'] = '../results/20250625_9h_20m_56s/block_allocation_result.xlsx'
 
     # 정반그룹 선호도 최소화 목적함수
     config['obj_preference'] = True  # True: 실행, False: 미실행
@@ -35,10 +35,10 @@ def create_config():
 
     # L/R 블록 배치 최대화 목적함수 (L/R 블록 배치 제약 허용 시 목적 함수 미실행 되도록 해야함)
     config['obj_allocation'] = True  # True: 실행, False: 미실행
-    config['weight_allocation'] = 100  # 가중치
-    config['score_position'] = 100  # 동일 정반 배치 시 위치 가중치
-    config['score_same_workarea'] = 100  # 동일 정반 배치 가중치
-    config['score_both_allocation'] = 100  # 타 정반 배치 가중치
+    config['weight_allocation'] = 10  # 가중치
+    config['score_position'] = 10  # 동일 정반 배치 시 위치 가중치
+    config['score_same_workarea'] = 10  # 동일 정반 배치 가중치
+    config['score_both_allocation'] = 10  # 타 정반 배치 가중치
 
     # 크레인 제약 활용 및 시간 설정
     config['crane_usage'] = True  # True: 실행, False: 미실행
