22d649c551f09c3d1abe20831cb7955907c7375e
Jiwon Baek <baekjiwon@snu.ac.kr>
2025-07-01 14:38:38 +0900

    Feat visualization

--- postprocessing/plot.py
+++ postprocessing/plot.py
@@ -75,6 +75,67 @@ def plot_workarea_group(fig, axes):
     axes[3].set_xlim([-50, 800])
     axes[3].set_ylim([-50, 300])
 
+def plot_integrated_workarea_group(fig, ax):
+    workarea_polygon = []
+    # 1번 정반그룹
+    workarea_polygon.append(generate_polygon_from_anchor(0, 350, 400, 75))
+
+    # 2번 정반그룹
+    workarea_polygon.append(generate_polygon_from_anchor(500, 350, 400, 100))
+
+    # 3번 정반그룹
+    workarea_polygon.append(generate_polygon_from_anchor(0, 0, 156, 250))
+
+    # 4번 정반그룹
+    neworigin_x = 250
+    workarea_polygon.append(generate_polygon_from_anchor(-10+neworigin_x, 0, 120, 100))
+    workarea_polygon.append(generate_polygon_from_anchor(110+neworigin_x, 0, 110, 245))
+    workarea_polygon.append(generate_polygon_from_anchor(325+neworigin_x, 0, 110, 260))
+    workarea_polygon.append(generate_polygon_from_anchor(435+neworigin_x, 0, 110, 260))
+    workarea_polygon.append(generate_polygon_from_anchor(660+neworigin_x, 0, 110, 255))
+    workarea_polygon.append(generate_polygon_from_anchor(740+neworigin_x, 0, 30, 70))
+
+    for workarea in workarea_polygon[:-1]:
+        ax.plot(*workarea.exterior.xy, color='grey')
+    ax.fill(*workarea_polygon[-1].exterior.xy, color='black')
+
+    margin_polygon = []
+    margin_polygon.append(generate_polygon_from_anchor(500, 435, 400, 15))
+    margin_polygon.append(generate_polygon_from_anchor(240, 0, 10, 100))
+    margin_polygon.append(generate_polygon_from_anchor(460, 0, 10, 245))
+    margin_polygon.append(generate_polygon_from_anchor(575, 0, 10, 260))
+    margin_polygon.append(generate_polygon_from_anchor(785, 0, 10, 260))
+    margin_polygon.append(generate_polygon_from_anchor(980, 0, 10, 70))
+    margin_polygon.append(generate_polygon_from_anchor(1010, 70, 10, 185))
+
+    for margin in margin_polygon:
+        ax.fill(*margin.exterior.xy, color='grey', alpha=0.5)
+
+def generate_integrated_polygon(groupidx, workareaidx, x, y, dx, dy):
+    if groupidx == 1:
+        y+=350
+    elif groupidx == 2:
+        x +=500
+        y +=350
+    elif groupidx == 3:
+        pass
+    elif groupidx == 4:
+        x+=250
+        if workareaidx == '(1,)':
+            x += -10
+        elif workareaidx == '(2, 3, 4)':
+            x += 110
+        elif workareaidx == '(5, 6, 7)':
+            x += 325
+        elif workareaidx == '(8, 9, 10)':
+            x += 435
+        elif workareaidx == '(11, 12, 13)':
+            x += 660
+
+
+    block_polygon = generate_polygon_from_anchor(x, y, dx, dy)
+    return block_polygon
+
 def generate_polygon(groupidx, workareaidx, x, y, dx, dy, rotate = False):
     groupidx = int(groupidx)
     if groupidx == 4:
@@ -95,6 +156,10 @@ def generate_polygon(groupidx, workareaidx, x, y, dx, dy, rotate = False):
     block_polygon = generate_polygon_from_anchor(x, y, dx, dy)
     return block_polygon
 
+def plot_integrated_block_polygon(fig, ax, _polygon):
+    ax.fill(*_polygon.exterior.xy, color='grey')
+    ax.plot(*_polygon.exterior.xy, color='black')
+
 def plot_block_polygon(fig, axes, _groupidx, _polygon, with_margin=True, color=None):
     if color is not None:
         axes[_groupidx - 1].fill(*_polygon.exterior.xy, color=color)
@@ -114,12 +179,18 @@ if __name__ == "__main__":
 
 
 
-    fig, axes = plt.subplots(nrows=2, ncols=2)
-    axes = axes.flatten()
-    plot_workarea_group(fig, axes)
-    plot_block_and_margin(fig, axes, groupidx=2, workareaidx=None, x=61, y=43, dx=38, dy=56, rotate=True, show_margin=True)
-    plot_block_and_margin(fig, axes, groupidx=2, workareaidx=None, x=0.0, y=0.0, dx=38, dy=56, rotate=True, show_margin=True)
+    # fig, axes = plt.subplots(nrows=2, ncols=2)
+    # axes = axes.flatten()
+    # plot_workarea_group(fig, axes)
+    # plot_block_and_margin(fig, axes, groupidx=2, workareaidx=None, x=61, y=43, dx=38, dy=56, rotate=True, show_margin=True)
+    # plot_block_and_margin(fig, axes, groupidx=2, workareaidx=None, x=0.0, y=0.0, dx=38, dy=56, rotate=True, show_margin=True)
+    #
+    # for ax in axes:
+    #     ax.set_aspect('equal')
+    # plt.show()
+    fig, ax = plt.subplots()
+    plot_integrated_workarea_group(fig, ax)
+    ax.set_aspect('equal')
 
-    for ax in axes:
-        ax.set_aspect('equal')
     plt.show()
+
---
+++ postprocessing/visualize.py
@@ -0,0 +1,117 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from shapely import intersection_all
+from shapely import intersection
+import cv2
+from postprocessing.plot import *
+import matplotlib.pyplot as plt
+import imageio
+import io
+import numpy as np
+from datetime import datetime
+
+class ScheduleChecker:
+    def __init__(self, schedule_path, block_path, save_gif):
+        self.schedule_path = schedule_path
+        self.block_path = block_path
+        self.block_info = pd.read_excel(self.block_path, sheet_name='BLK', skiprows=[1])
+        self.workarea_info = pd.read_excel(self.block_path, sheet_name='WORKAREA_GROUP', skiprows=[1])
+        self.raw_schedule = pd.read_excel(self.schedule_path, sheet_name='크레인 정보', skiprows=[1])
+        print("Schedule loaded successfully!")
+        self.prefix = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+        self.scheduled = self.raw_schedule[self.raw_schedule['배치확정여부'].notna()]
+        dates = pd.concat([self.scheduled['착수일'].dt.date, self.scheduled['완료일'].dt.date]).dropna().unique()
+        time_horizon = sorted(dates)
+        self.min_date = time_horizon[0]
+        self.max_date = time_horizon[-1]
+        print(f"time horizon: {self.min_date} ~ {self.max_date}")
+
+        # 전체 연속 날짜 리스트로 생성 (중간 날짜 포함)
+        self.time_horizon = pd.date_range(start=self.min_date, end=self.max_date).date.tolist()
+
+        self.image_list = []
+        self.blocks_by_time_dict = {}
+        self.intersections_by_time_dict = {}
+        self.summary = {}
+
+        self._create_polygon()
+
+        if save_gif:
+            self.create_GIF()
+
+    def _create_polygon(self):
+        for t in self.time_horizon:
+            timestamp_t = pd.Timestamp(t)
+            presence = self.scheduled[
+                (self.scheduled['착수일'] <= timestamp_t) &
+                (self.scheduled['완료일'] >= timestamp_t)
+                ]
+
+            self.blocks_by_time_dict[t] = []
+            for idx, row in presence.iterrows():
+                groupidx = int(row['그룹ID'][1:-1].split(', ')[0])
+                workareaidx = '('+row['그룹ID'][1:-1].split(', (')[1]
+                poly = generate_integrated_polygon(groupidx=groupidx,
+                                                   workareaidx=workareaidx,
+                                      x=row['블록위치X']*10, y=row['블록위치Y'] * 10,
+                                      dx=row['변환 블록길이'] * 10, dy=row['변환 블록폭'] * 10)
+                self.blocks_by_time_dict[t].append(poly)
+
+
+    def _create_image(self, t):
+        fig, ax = plt.subplots()
+
+        plot_integrated_workarea_group(fig, ax)
+        ax.set_aspect('equal')
+        for idx, block_polygon in enumerate(self.blocks_by_time_dict[t]):
+            plot_integrated_block_polygon(fig, ax, block_polygon)
+
+        fig.suptitle(t)
+        # 4. fig를 메모리 상의 이미지로 저장
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        image = imageio.v2.imread(buf)
+        self.image_list.append(image)
+        plt.close(fig)  # 메모리 누수 방지
+
+    def create_GIF(self):
+        for t in self.blocks_by_time_dict.keys():
+            self._create_image(t=t)
+
+        # 5. GIF 저장
+        height, width, _ = self.image_list[0].shape
+        black_frame = np.dstack([np.zeros((height, width, 3), dtype=np.uint8),
+                                 np.full((height, width), 255, dtype=np.uint8)])        # 2. 검은 화면 삽입
+        self.image_list.append(black_frame)
+
+        # 3. GIF 저장 (마지막 프레임만 길게 보여줌)
+        durations = [0.5] * (len(self.image_list)-1)  # 마지막 검은 프레임을 1.5초 보여줌
+        durations.append(0.1)
+
+        # imageio.imwrite('black_frame.png', black_frame)
+        # imageio.mimsave(self.prefix + ".gif", self.image_list, duration=durations, loop=0)  # duration은 프레임 간 시간(초)
+        # 저장된 이미지들로 GIF 생성
+        # imageio.v3.imwrite(self.prefix + ".gif", self.image_list, duration=durations, loop=0)
+
+        h, w, _ = self.image_list[0].shape
+        fps = 1  # 1 fps
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # 플랫폼 호환성 ↑
+        out = cv2.VideoWriter(self.prefix + ".mp4", fourcc, fps, (w, h))
+
+        for frame in self.image_list:
+            # BGRA → BGR
+            if frame.shape[2] == 4:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+            out.write(frame.astype(np.uint8))
+
+        out.release()
+
+
+if __name__ == "__main__":
+    # OpenCV 라이브러리를 설치해야 함 (conda install openCV 사용)
+    schedule_path = "../data/General Case_1.xlsx"
+    block_path = "../data/data_rev0.2.xlsx"
+
+    checker = ScheduleChecker(schedule_path, block_path, save_gif=True)
